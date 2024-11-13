6947129de5a79db011ede3a4c5a8004adb52b2d4
John Oye <olugbengajohnoye@gmail.com>
2024-11-13 13:10:46 +0100

    UPDATE: removed need for join requests, now saves messages in Redis

--- src/main/java/com/johnoye742/archgabriel/TextMessageHandler.java
+++ src/main/java/com/johnoye742/archgabriel/TextMessageHandler.java
@@ -5,10 +5,13 @@ import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
+import redis.clients.jedis.UnifiedJedis;
 
 import java.io.IOException;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class TextMessageHandler extends TextWebSocketHandler {
 
@@ -17,25 +20,26 @@ public class TextMessageHandler extends TextWebSocketHandler {
     @Override
     public void afterConnectionEstablished(WebSocketSession session) {
         // After connection is established add the session to the clients set
-        session.getUri();
+        System.out.println(session.getUri().getPath());
+        clients.computeIfAbsent(Objects.requireNonNull(session.getUri()).getPath(), _ -> new HashSet<>()).add(session);
     }
     @Override
     public void handleTextMessage(WebSocketSession session, TextMessage message) throws IOException {
-        Gson gson = new Gson();
+    Gson gson = new Gson();
         JsonData data = gson.fromJson(message.getPayload(), JsonData.class);
         System.out.println(message.getPayload());
 
-        if(data.getType().equals("join")) {
-            clients.computeIfAbsent(data.getRoomId(), k -> new HashSet<>()).add(session);
-        }
-
         if(data.getType().equals("message")) {
-            Set<WebSocketSession> connectedClients = clients.get(data.getRoomId());
+            Set<WebSocketSession> connectedClients = clients.get(Objects.requireNonNull(session.getUri()).getPath());
             // Do not send if not a member of the room
-            if(connectedClients.contains(session)) {
-                for(WebSocketSession ses : connectedClients) {
-                    if(ses.isOpen())
-                        ses.sendMessage(message);
+            try (UnifiedJedis redis = new UnifiedJedis("redis://localhost:6379")) {
+                if (connectedClients.contains(session)) {
+                    redis.lpush(Objects.requireNonNull(session.getUri()).getPath(), message.getPayload());
+                    for (WebSocketSession ses : connectedClients) {
+                        if (ses.isOpen()) {
+                            ses.sendMessage(message);
+                        }
+                    }
                 }
             }
         }
